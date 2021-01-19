52bdef7951d1eb4008f274787aaa5f66c369b640
lucas baiba <lucasbaiba@MacBook-Pro-de-lucas.local>
2021-01-19 10:39:06 +0100

    Add Sports/Trips Page (BASE) + base model design SCSS

--- outsiders-front/src/components/Sports/sports.js
+++ outsiders-front/src/components/Sports/sports.js
@@ -1,11 +1,50 @@
 import React from 'react';
 
 
+// import styles
+
+import './sport.scss';
+
+
 
 const Sports = () => {
 
     return (
-        <div> SPORT PAGE</div>
+        <div className='sport_container'> 
+        
+        <div className='sport_card'>
+            <h1>CATEGORIES</h1>
+            <ul>
+                <li>Sport</li>
+                <li>Sport</li>
+                <li>Sport</li>
+            </ul>
+        </div>
+        <div className='sport_card'>
+            <h1>CATEGORIES</h1>
+            <ul>
+                <li>Sport</li>
+                <li>Sport</li>
+                <li>Sport</li>
+            </ul>
+        </div>
+        <div className='sport_card'>
+            <h1>CATEGORIES</h1>
+            <ul>
+                <li>Sport</li>
+                <li>Sport</li>
+                <li>Sport</li>
+            </ul>
+        </div>
+        <div className='sport_card'>
+            <h1>CATEGORIES</h1>
+            <ul>
+                <li>Sport</li>
+                <li>Sport</li>
+                <li>Sport</li>
+            </ul>
+        </div>
+        </div>
     )
 }
 
--- outsiders-front/src/components/Trips/trips.js
+++ outsiders-front/src/components/Trips/trips.js
@@ -1,11 +1,64 @@
 import React from 'react';
 
 
+import './trips.scss';
+
+
 
 const Trips = () => {
     
     return (
-        <div> TRIPS DETAILS </div>
+        <div className='trips_container'>
+            <div className='trips_card'>
+
+                <div className='sport'>SPORT</div>
+                <div className='date'>DATE</div>
+                <div className='durantion'>1/2DAY</div>
+                <div className='peoples'>PPL</div>
+                <div className='destination'>SPORT</div>
+                <div className='departure'>DATE</div>
+                <div className='price'>€</div>
+                <div className='profil'>profil</div>
+
+            </div>
+            <div className='trips_card'>
+
+                <div className='sport'>SPORT</div>
+                <div className='date'>DATE</div>
+                <div className='durantion'>1/2DAY</div>
+                <div className='peoples'>PPL</div>
+                <div className='destination'>SPORT</div>
+                <div className='departure'>DATE</div>
+                <div className='price'>€</div>
+                <div className='profil'>profil</div>
+
+            </div>
+            <div className='trips_card'>
+
+                <div className='sport'>SPORT</div>
+                <div className='date'>DATE</div>
+                <div className='durantion'>1/2DAY</div>
+                <div className='peoples'>PPL</div>
+                <div className='destination'>SPORT</div>
+                <div className='departure'>DATE</div>
+                <div className='price'>€</div>
+                <div className='profil'>profil</div>
+
+            </div>
+            <div className='trips_card'>
+
+                <div className='sport'>SPORT</div>
+                <div className='date'>DATE</div>
+                <div className='durantion'>1/2DAY</div>
+                <div className='peoples'>PPL</div>
+                <div className='destination'>SPORT</div>
+                <div className='departure'>DATE</div>
+                <div className='price'>€</div>
+                <div className='profil'>profil</div>
+
+            </div>
+        
+        </div>
     );
 }
 
