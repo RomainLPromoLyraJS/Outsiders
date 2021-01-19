ce2dd82f42e6899389673fabc656f0806f738bf8
mathieupromolyra <mat.sr0575@gmail.com>
2021-01-19 16:57:47 +0100

    import valeurs

---
+++ outsiders-back/import/import.sql
@@ -0,0 +1,43 @@
+INSERT INTO role("name") VALUES 
+("member"),
+("administrator");
+
+INSERT INTO user("lastname", "firstname", "mail", "password", "pseudo", "description") VALUES 
+("");
+
+INSERT INTO category(title, "description") VALUES
+("Eau", ),
+("Air", ),
+("Terre", ),
+("Neige", );
+
+INSERT INTO trip("title", "description", "date", "time", "from", "to", "places", "minimum", "price", "duration") VALUES
+(""),
+
+INSERT INTO sport(title, "description") VALUES 
+("Canoë-kayak", ),
+("Canyoning", ),
+("Hydro-speed", ),
+("Kite-surf", ),
+("Paddleboarding", ),
+("Plongée",),
+("Rafting"),
+("Snorkeling", ),
+("Surf", ),
+("Nautisme à la voile", ),
+("Windsurf", ),
+("Wake-board", ),
+("Parapente", ),
+("Base-jump", ),
+("ULM", ),
+("Enduro", ),
+("Escalade", ),
+("Rando", ),
+("Trail", ),
+("VTT", ),
+("Ski", ),
+("Ski de fond", ),
+("Snowboard", );
+
+INSERT 
+
