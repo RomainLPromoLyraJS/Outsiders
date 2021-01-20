c957cd88cfef3eed8e4c9e597f94c104f3e30eb8
RomainLPromoLyraJS <romain75l@gmail.com>
2021-01-20 09:34:51 +0100

    Scss fixed

--- outsiders-front/src/components/Login/index.js
+++ outsiders-front/src/components/Login/index.js
@@ -7,8 +7,8 @@ const Login = () => {
         <div className='login-page'>
             <p className='login-page__description'>Si vous êtes membre du site, veuillez vous connecter :</p>
            <form className='login-page__form'>
-                 <label className='login-page__form__label' htmlFor='email'>Identifiant</label>
-                <input className='login-page__form__input' type='email' name='login' id='login' placeholder='Saisissez votre e-mail'/>
+                <label className='login-page__form__label' htmlFor='email'>Identifiant</label>
+                <input className='login-page__form__input' type='email' name='mail' id='mail' placeholder='Saisissez votre e-mail'/>
                 <label className='login-page__form__label' htmlFor='password'>Mot de passe</label>
                 <input className='login-page__form__input' type='password' name='password' id='password' placeholder='Saisissez votre mot de passe'/>
                 <button className='login-page__form__button' type='submit' name='button' id='button'>Valider</button>
