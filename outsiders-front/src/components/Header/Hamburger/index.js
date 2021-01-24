7c3f96657a1d81eb85eaeb0fed3ce2fcabbdb402
LeoJoly <joly.leo@gmail.com>
2021-01-24 12:49:40 +0100

    open and close animation + global css fix

--- outsiders-front/src/components/Header/Hamburger/index.js
+++ outsiders-front/src/components/Header/Hamburger/index.js
@@ -3,6 +3,7 @@ import React, { useEffect, useRef } from 'react';
 import { NavLink } from 'react-router-dom';
 import { IoSearch, IoAddCircleOutline } from "react-icons/io5";
 import { IoIosCloseCircle } from "react-icons/io";
+import { gsap } from "gsap";
 
 // == Local imports
 // images
@@ -10,24 +11,60 @@ import outsidersLogo from '../../../assets/logos/Outsiders_LOGOS-line_WHITE.svg'
 //components
 
 const Hamburger = ({ burgerState, toggleMenu, disabledState }) => {
+  // animated dom nodes
   let menu = useRef(null);
-
+  let revealMenu = useRef(null);
+  let revealMenuBG = useRef(null);
+  
+  // toggle open and close
   useEffect(() => {
     if (burgerState.clicked === false) {
       // close the menu
-      menu.style.display = "none";
-
+      gsap.to([revealMenu, revealMenuBG], {
+        duration: .8,
+        height: 0,
+        ease: 'power3.inOut',
+        stagger: {
+          amount: .07
+        }
+      });
+      gsap.to(menu, {
+        duration: 1,
+        css: {display: "none"},
+      });
+      
     } else if (burgerState.clicked === true || (burgerState.clicked === true && burgerState.init === null)) {
       // open the menu
-      menu.style.display = "block";
-
+      gsap.to(menu, {
+        duration: 0,
+        css: {display: "block"},
+      });
+      gsap.to([revealMenuBG, revealMenu], {
+        duration: 0,
+        opacity: 1,
+        height: "100%",
+      });
+      staggerOpenning(revealMenuBG, revealMenu);
     }
   }, [burgerState]);
 
+  const staggerOpenning = (node1, node2) => {
+    gsap.from([node1, node2], {
+      duration: .8,
+      height: 0,
+      transformOrigin: "right top",
+      skewY: 2,
+      ease: "poxer3.inOut",
+      stagger: {
+        amount: 0.1,
+      }
+    });
+  };
+
   return (
     <div ref={el => (menu = el)} className="hamburger">
-      <div className="hamburger__bg" />
-      <div className="menu">
+      <div ref={el => (revealMenuBG = el)} className="hamburger__bg" />
+      <div ref={el => (revealMenu = el)} className="menu">
         <div className="menu__category__BG"></div>
         <div className="menu__container">
           <div className="menu__container__header">
