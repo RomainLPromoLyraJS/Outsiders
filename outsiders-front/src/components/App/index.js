863c48284a51d40891e1fb69fd2a1cf2e7d594f9
lucas baiba <lucasbaiba@iMac-de-lucas.local>
2021-01-24 15:22:08 +0100

    HoC withRouter OK retrieve trip.data by slug title and trip title

--- outsiders-front/src/components/App/index.js
+++ outsiders-front/src/components/App/index.js
@@ -12,7 +12,7 @@ import Trips from '../../containers/Trip';
 import Login from '../../containers/Login';
 import About from '../About';
 import Signup from '../../containers/Signup';
-import Tripdetails from '../Tripdetails';
+import Tripdetails from '../../containers/Tripdetails';
 import Admin from '../../containers/Admin';
 import Profile from '../../containers/Profile';
 
--- outsiders-front/src/utils.js
+++ outsiders-front/src/utils.js
@@ -1,6 +1,22 @@
 // Import package
 import slugify from 'slugify';
 
-
 // Create function to replace URL:id for URL:trip.title
-export const buildTripURL = (title) => `/sortie/${slugify(title, { lower: true })}`;
+// Transform trip title into slug
+export const slugifyTitle = (title) => slugify(title, { lower: true });
+
+//Adding /route before slugifyTitle
+export const buildTripURL = (title) => `/sortie/${slugifyTitle(title)}`;
+
+/** 
+ * --FIND TRIP BY SLUG--
+ * 
+ * trip.title === (SLUG)trip-title
+ */
+
+ export const getTripBySlug = (trips, slug) => (
+   trips.find((trip) => slugifyTitle(trip.title) === slug)
+ );
+
+
+
