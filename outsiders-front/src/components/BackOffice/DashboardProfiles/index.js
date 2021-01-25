506fa3355c0d779d1d856ebcefa3d56ca3348207
RomainLPromoLyraJS <romain75l@gmail.com>
2021-01-25 10:42:30 +0100

    Modifications static datas

--- outsiders-front/src/components/BackOffice/DashboardProfiles/index.js
+++ outsiders-front/src/components/BackOffice/DashboardProfiles/index.js
@@ -1,49 +1,58 @@
 // == Package imports
-import React from 'react';
+import React from "react";
 
 // == Local imports
-import DashboardNav from '../DashboardNav';
-
+import DashboardNav from "../DashboardNav";
 
 const DashboardProfiles = () => (
   <>
     <div className="dashboard">
-    <DashboardNav />
-   <div className="dashboard-profiles">
-  <form>
-      <div className='dashboard-profiles__form'>
-      <h3>Créer un profil</h3>
-      <label forhtml='Nom du profil'></label>
-      <input type='text' className='dashboard-profiles__form__input'></input>
-      <button type="submit" className='dashboard-profiles__form__button'>Valider</button>
+      <DashboardNav />
+      <div className="dashboard-profiles">
+        <form>
+          <div className="dashboard-profiles__form">
+            <h3>Créer un profil</h3>
+            <label htmlFor="create-profile">Nom du profil </label>
+            <input
+              type="text"
+              className="dashboard-profiles__form__input"
+            ></input>
+            <button type="submit" className="dashboard-profiles__form__button">
+              Valider
+            </button>
+          </div>
+          <div className="dashboard-profiles__form">
+            <h3>Modifier un profil</h3>
+            <label htmlFor="modify-profile">
+              Sélectionner le profil à modifier dans la liste :
+            </label>
+            <select name="profile" className="dashboard-profiles__form__select">
+              <option value="">Profils</option>
+              <option value="">Profil 1</option>
+              <option value="">Profil 2</option>
+            </select>
+            <button type="submit" className="dashboard-profiles__form__button">
+              Valider
+            </button>
+          </div>
+          <div className="dashboard-profiles__form">
+            <h3>Supprimer un profil</h3>
+            <label htmlFor="delete-profile">
+              Sélectionner le profil à supprimer dans la liste :
+            </label>
+            <select name="profile" className="dashboard-profiles__form__select">
+              <option value="">Profils</option>
+              <option value="">Profil 1</option>
+              <option value="">Profil 2</option>
+            </select>
+            <button type="submit" className="dashboard-profiles__form__button">
+              Valider
+            </button>
+          </div>
+        </form>
       </div>
-      <div className='dashboard-profiles__form'>
-      <h3>Modifier un profil</h3>
-      <label forhtml='Sélectionner le profil à modifier dans la liste :'></label>
-      <select name='profile' className='dashboard-profiles__form__select'>
-      <option value=''>profiles</option>
-      <option value=''>Surf</option>
-      <option value=''>Paddle</option>
-      </select>
-      <button type="submit" className='dashboard-profiles__form__button'>Valider</button>
-      </div>
-      <div className='dashboard-profiles__form'>
-      <h3>Supprimer un profil</h3>
-      <select name='profile' className='dashboard-profiles__form__select'>
-      <option value=''>profiles</option>
-      <option value=''>Surf</option>
-      <option value=''>Paddle</option>
-      </select>
-      <label forhtml='Sélectionner le profil à supprimer dans la liste :'></label>
-      <button type="submit" className='dashboard-profiles__form__button'>Valider</button>
-      </div>
-      </form>
-     </div>
-     </div>
-  
-
+    </div>
   </>
-  
 );
 
 export default DashboardProfiles;
