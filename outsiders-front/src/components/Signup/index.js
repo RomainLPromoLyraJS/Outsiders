2cb56159c4c7ad7bc384e3041f5487ca6c87bce9
LeoJoly <joly.leo@gmail.com>
2021-01-26 12:31:26 +0100

    signup page scss styling

--- outsiders-front/src/components/Signup/index.js
+++ outsiders-front/src/components/Signup/index.js
@@ -1,90 +1,61 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Signup = ({
-  firstnameValue,
-  lastnameValue,
-  usernameValue,
-  emailValue,
-  passwordValue,
-  descriptionValue,
-  handleChange
-}) => {
-  const onChange = (event) => {
-      handleChange(event.target.value, event.target.name);
-  };
-  
+const Signup = () => {
   return (
-    <div className='signup'>
-      <form className="signup_form">
-        <h1 className="signup_title">Sign up</h1>
+    <div className="signup">
+      <h1 className="signup__title">Inscription</h1>
+      <form className="signup__form">
+        <label className="signup__form__label" htmlFor="fisrtname">Prénom</label>
         <input
-          className='signup_form_input'
-          placeholder='FirstName'
-          type='text'
-          name='firstname'
-          value={firstnameValue}
-          onChange={onChange}
+          className="signup__form__input"
+          type="text"
+          name="firstname"
+          id="firstname"
+          placeholder="Kelly"
         />
+        <label className="signup__form__label" htmlFor="lastname">Nom de famille</label>
         <input
-          className='signup_form_input'
-          placeholder='LastName'
-          type='text'
-          name='lastname'
-          value={lastnameValue}
-          onChange={onChange}
+          className="signup__form__input"
+          type="text"
+          name="lastname"
+          id="lastname"
+          placeholder="Slater"
         />
+        <label className="signup__form__label" htmlFor="username">Nom d'utilisateur</label>
         <input
-          className='signup_form_input'
-          placeholder='UserName'
-          type='text'
-          name='username'
-          value={usernameValue}
-          onChange={onChange}
+          className="signup__form__input"
+          type="text"
+          name="username"
+          id="username"
+          placeholder="Surf Master"
         />
+        <label className="signup__form__label" htmlFor="email">Email</label>
         <input
-					className='signup_form_input'
-					placeholder='Email'
-					type='email'
-					name='email'
-					value={emailValue}
-					onChange={onChange}
+          className="signup__form__input"
+          type="email"
+          name="email"
+          id="email"
+          placeholder="kelly.@slater.surf"
         />
+        <label className="signup__form__label" htmlFor="password">Mot de passe</label>
         <input
-          className='signup_form_input'
-          placeholder='Password'
-          type='password'
-          name='password'
-          value={passwordValue}
-          onChange={onChange}
+          className="signup__form__input"
+          type="password"
+          name="password"
+          id="password"
+          placeholder="min 8 caractères (a, A, 2, *)"
         />
-        <input
-          className='signup_form_input'
-          placeholder='ShortDescription'
-          type='text'
-          name='description'
-          value={descriptionValue}
-          onChange={onChange}
+        <label className="signup__form__label" htmlFor="description">Mot de passe</label>
+        <textarea
+          id="description"
+          className="signup__form__textarea"
+          placeholder="C'est le moment de se décrire un petit peu..."
         />
-        <button
-          type='submit'
-          className='signup_form_button'
-        >
-          SignUp
-        </button>
+        <button className="signup__form__button" type="submit">Valider !</button>
       </form>
     </div>
-  )
-};
-
-Signup.propTypes = { 
-  lastnameValue: PropTypes.string.isRequired,
-  firstnameValue: PropTypes.string.isRequired,
-  usernameValue: PropTypes.string.isRequired,  
-  emailValue: PropTypes.string.isRequired,
-  passwordValue: PropTypes.string.isRequired,
-  descriptionValue: PropTypes.string.isRequired,
-  handleChange: PropTypes.func.isRequired,
+  );
 };
 
 export default Signup;
