728116c07cd490c113a7859c935af2046a22b633
mathieupromolyra <mat.sr0575@gmail.com>
2021-01-29 18:35:17 +0100

    implementation controle admin et role

--- outsiders-back/app/middleware/admin.js
+++ outsiders-back/app/middleware/admin.js
@@ -1,14 +1,23 @@
+const jwt = require('jsonwebtoken');
+
+//mon petit MW de d'authentification administrateur
 const adminMW = (req, res, next) => {
-    
-    if (!req.session.user) {
-        return response.redirect('/login');
-    }
-    
-    if (req.session.user.role === 'admin') {
-        return next();
-    }
-    
-    res.render('401');
-}
+
+    try {
+        const token = req.headers.authorization.split(' ');
+        const tokenDecoded = jwt.verify(token[1], process.env.JWTSECRET);
+        const tokenRoleId = tokenDecoded.roleId;
+        if (tokenRoleId === 2) {
+            console.log('Accès autorisé : connecté en administrateur');
+            return next();
+        } else {
+            console.log('Accès interdit : il faut être administrateur')
+            res.status('403').json({message : 'Accès interdit : il faut être administrateur'});
+            next(error);
+        }
+    } catch (error) {
+            next(error);
+        }
+};
 
 module.exports = adminMW;
