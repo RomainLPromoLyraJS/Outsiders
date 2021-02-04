21e9c67cb28339c81c73f39b8b3f1e417d1c4a33
mathieupromolyra <mat.sr0575@gmail.com>
2021-02-05 00:04:43 +0100

    upload

--- outsiders-back/app.js
+++ outsiders-back/app.js
@@ -4,6 +4,9 @@ const bodyParser = require('body-parser');
 const jwt = require('express-jwt');
 const jsonwebtoken = require('jsonwebtoken');
 
+//const path = require('path');
+const fileUpload = require ('express-fileupload');
+
 const authMiddleware = require('./app/middleware/auth');
 
 const cors = require('cors');
@@ -15,7 +18,39 @@ const app = express();
 app.use(express.json());
 app.use(cors('*'));
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
+
+//autorise la création d'un nouveau dossier s'il n'existe pas lors de l'upload
+app.use(fileUpload({
+    createParentPath: true,
+    limits: { fileSize: 50 * 1024 * 1024 }
+}));
+
+app.post('/upload', function(req, res) {
+    let sampleFile;
+    let uploadPath;
+
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).send('No files were uploaded.');
+    };
+
+    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+    sampleFile = req.files.sampleFile;
+    uploadPath = __dirname + '/public' + sampleFile.name;
+
+    // Use the mv() method to place the file somewhere on your server
+    sampleFile.mv(uploadPath, function(err) {
+        console.log('photo');
+        if (err) {
+            return res.status(500).send(err)
+        };
+            res.send('File uploaded!')
+    })
+});
+
+//app.use(express.static(path.join(_dirname, 'public')));
+
+app.use(bodyParser.urlencoded({extended: true})); //false?
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
